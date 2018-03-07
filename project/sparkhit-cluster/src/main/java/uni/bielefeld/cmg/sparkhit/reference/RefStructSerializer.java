94972dbe189760940a9decd6471f3f663db97310
rhinempi <huanglr@cebitec.uni-bielefeld.de>
2018-03-07 13:49:47 +0100

    Sparkhit updated 1.0 version

--- project/sparkhit-cluster/src/main/java/uni/bielefeld/cmg/sparkhit/reference/RefStructSerializer.java
+++ project/sparkhit-cluster/src/main/java/uni/bielefeld/cmg/sparkhit/reference/RefStructSerializer.java
@@ -18,26 +18,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Liren Huang on 17/02/16.
+ * Created by rhinempi on 22.07.2017.
  *
- *      spark-hit_standalone
+ *       Sparkhit
  *
- * Copyright (c) 2015-2015
- *      Liren Huang      <huanglr at cebitec.uni-bielefeld.de>
- * 
- * spark-hit_standalone is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * Copyright (c) 2017.
+ *       Liren Huang     <huanglr at cebitec.uni-bielefeld.de>
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; Without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more detail.
- * 
- * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <http://www.gnu.org/licenses>.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 /**
